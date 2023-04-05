fa98c1a3b7f7450c6eef51ccfff7e32921603c23
abdelghani draoui <draoui.abdelghani@gmail.com>
2023-04-05 12:21:22 +0200

    importing spring core , spring beans spring context modules using maven

---
+++ src/main/java/dz/cut/presentation/InjectionSpringViaXMLFile.java
@@ -0,0 +1,16 @@
+package dz.cut.presentation;
+
+import dz.cut.business.Business;
+import dz.cut.dao.IDao;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
+public class InjectionSpringViaXMLFile {
+    public static void main(String[] args) throws Exception {
+
+
+
+    }
+}
